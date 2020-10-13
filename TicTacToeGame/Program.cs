da437e130e99fc629ae68dcab9a71f5069c28977
Snehil <snehilverma488@gmail.com>
2020-10-13 09:45:33 +0530

    [Snehil] Add : Method to Create TicTacToe Game Board

---
+++ TicTacToeGame/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TicTacToeGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TicTacToeGame ticTacToe = new TicTacToeGame();
+            char[] gameBoard = ticTacToe.CreateBoard();
+        }
+    }
+}
---
+++ TicTacToeGame/TicTacToeGame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToeGame
+{
+    class TicTacToeGame
+    {
+        public char[] CreateBoard()
+        {
+            char[] gameBoard = new char[10];
+            for (int block = 1; block < 10; block++)
+            {
+                gameBoard[block] = ' ';
+            }
+            return gameBoard;
+        }
+    }
+}
